152ab0ef939209a1cdf04c3ff12594884e0a9c76
churin <wl00796865@gmail.com>
2024-11-02 18:17:47 +0800

    add dotenv, connect to db

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');  // to use .env, if variables are included, also need to add dotenv-expand
+dotenv.config();
 const app = express();
-const PORT = 3000;
 mongoose.set('strictQuery', false);
 
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.CONNECT_URL
 
 app.use(express.json());  // remember to add this or the result will be undefined
 app.use(express.urlencoded({ extended: true }));  // remember to add this or the result will be undefined
@@ -36,7 +39,7 @@ app.post('/api/customers', (req, res) => {
 
 const start = async() => {
     try{
-        await mongoose.connect();
+        await mongoose.connect(MONGO_URI);
         app.listen(PORT, () => {
             console.log(`Server is listening on port ${PORT}`);
         })
