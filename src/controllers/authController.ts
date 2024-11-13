c608767a2fea108c1870fd6da72df1ed43fc9eb7
churin <wl00796865@gmail.com>
2024-11-13 09:52:46 +0800

    signup with cookies

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { User } from "../models/user";
+import jwt from 'jsonwebtoken';
+import { Types } from "mongoose";
 
 
 interface ValidationError {
@@ -26,6 +28,12 @@ const handleErrors = (err: ValidationError) => {
     return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id: Types.ObjectId) => {
+    return jwt.sign({id}, 'any secret key', {
+        expiresIn: maxAge  // 3 days
+    })
+}
 
 const signup_get = (req: Request, res: Response) => {
     res.render('auths/signup', {title: 'Sign Up'});
@@ -38,7 +46,11 @@ const login_get = (req: Request, res: Response) => {
 const signup_post = (req: Request, res: Response) => {
     const { email, password } = req.body;
     User.create({email, password})
-    .then((user) => res.status(201).json(user))
+    .then((user) => {
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000}); // 3 days in ms
+        res.status(201).json({user: user._id});
+    })  
     .catch((e) => {
         const errors = handleErrors(e);
         res.status(400).json({error: errors})
